1885249f068af8d11357c5066a39fae84fa4aea8
unknown <omor4498@gmail.com>
2023-09-01 20:31:37 +0600

    first commit

---
+++ js/java.js
@@ -0,0 +1,48 @@
+// fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+//       .then(res => res.json())
+//       .then(data => displayData(data))
+
+
+// function displayData(post) {
+//       for (const AllData in post) {
+//             console.log(AllData);
+//             }
+// };
+
+const loadPhone = async () => {
+      const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+      const data = await res.json();
+      // console.log(data);
+      displayPhones(data.data);
+
+};
+const displayPhones = phones => {
+      const phoneContainer = document.getElementById('phone-container');
+      phones.forEach(phone =>{
+            console.log(phone);
+            //Create a div
+            const phoneCard = document.createElement('div');
+            phoneCard.classList = `card w-full bg-base-100 shadow-xl`
+            phoneCard.innerHTML =
+                  `
+                   <figure class="px-10 pt-10">
+                                    <img src="${phone.image}" alt="Shoes" class="rounded-xl " />
+                              </figure>
+                              <div class="card-body items-center text-center">
+                                    <h2 class="card-title">${phone.phone_name}</h2>
+                                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                                    <div class="card-actions">
+                                          <button class="btn btn-primary">Buy Now</button>
+                                    </div>
+                              </div>
+                  `;
+            phoneContainer.appendChild(phoneCard);
+            
+
+      })
+}
+
+
+
+
+loadPhone();
